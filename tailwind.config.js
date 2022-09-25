650431abba25f5d7be62032a909aaaee2185573b
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-09-25 12:45:51 +0530

    School Website

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./pages/students.html"],
+  theme: {
+    extend: {
+      colors : {
+        'bgDark' : '#4C0033',
+      },
+    },
+  },
+  plugins: [],
+}
